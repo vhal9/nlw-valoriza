c5af511bd27ba8901d6ddd931181508a4e5c0c66
Victor Hugo Landin <victor.h.landin@gmail.com>
2021-08-23 21:20:12 -0300

    adiciona middleware para verificar autenticação do usuário

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+    sub: string;
+}
+
+export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+
+    const authToken = request.headers.authorization;
+    
+    if (!authToken) {
+        return response.status(401).end();
+    }
+
+    const [,token] = authToken.split(" ");
+    
+    try {
+
+        const { sub } = verify(token, "b5017cc5298636a71169ff5175383ecc") as IPayload;
+
+        return next();
+    
+    } catch (err) {
+
+        return response.status(401).end();
+
+    }   
+
+}
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 
 const router = Router();
 
@@ -12,9 +13,9 @@ const createTagController = new CreateTagController();
 const createComplimentController = new CreateComplimentController;
 const authenticateUserController = new AuthenticateUserController();
 
-router.post("/users", createUserController.handle);
-router.post("/tags", ensureAdmin, createTagController.handle);
+router.post("/users", ensureAuthenticated, createUserController.handle);
+router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle);
 router.post("/login", authenticateUserController.handle);
-router.post("/compliments", createComplimentController.handle);
+router.post("/compliments", ensureAuthenticated, createComplimentController.handle);
 
 export { router };
