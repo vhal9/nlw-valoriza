73f011482dba697b59c2cb995333a1d6a5da98dd
Victor Hugo Landin <victor.h.landin@gmail.com>
2021-08-24 18:15:11 -0300

    refatoração para deixar mais organizado a declaração das rotas

--- src/routes.ts
+++ src/routes.ts
@@ -12,27 +12,31 @@ import { ListUsersController } from "./controllers/ListUsersController";
 
 const router = Router();
 
-const createUserController = new CreateUserController();
-const createTagController = new CreateTagController();
-const createComplimentController = new CreateComplimentController();
 const authenticateUserController = new AuthenticateUserController();
+
+const createUserController = new CreateUserController();
+const listUsersController = new ListUsersController();
+
+const createTagController = new CreateTagController();
+const listTagsController = new ListTagsController();
+
+const createComplimentController = new CreateComplimentController();
 
 const listUserSendComplimentsController = new ListUserSendComplimentsController();
 const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
 
-const listTagsController = new ListTagsController();
 
-const listUsersController = new ListUsersController();
-
-router.post("/users", ensureAuthenticated, createUserController.handle);
-router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle);
 router.post("/login", authenticateUserController.handle);
-router.post("/compliments", ensureAuthenticated, createComplimentController.handle);
+
+router.post("/users", createUserController.handle);
+router.get("/users/", ensureAuthenticated, listUsersController.handle);
 
 router.get("/users/compliments/send", ensureAuthenticated, listUserSendComplimentsController.handle);
 router.get("/users/compliments/receive", ensureAuthenticated, listUserReceiveComplimentsController.handle);
 
+router.post("/compliments", ensureAuthenticated, createComplimentController.handle);
+
+router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle);
 router.get("/tags", listTagsController.handle);
-router.get("/users/", ensureAuthenticated, listUsersController.handle);
 
 export { router };
